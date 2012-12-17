9c7f0ff4ad447b5745e14c26d60fc24d0eabfdfc
alexcweiner <alexcweiner@gmail.com>
2012-12-17 16:40:21 -0500

    added command line option to select distribution

--- distributions.c
+++ distributions.c
@@ -1,12 +1,14 @@
 /***
 This program generates the sequences used for branch prediction
+TODO: Add command line option to set the number of samples
 ***/
 #include <stdio.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
-int main (void)
+int main(int argc, char *argv[])
 {
+int selector;
 const gsl_rng_type * T;
 gsl_rng * r;
 int i;
@@ -18,13 +20,30 @@ gsl_rng_env_setup();
 T = gsl_rng_default;
 r = gsl_rng_alloc (T);
 
-for (i = 0; i < n; i++)
-{
-	unsigned int k = gsl_ran_poisson(r,mu);
-	printf(" %u",k);
-	
+for (selector = 1; selector < argc; selector++)  /* Skip argv[0] (program name). */
+	{
 
-}
+	//select the poisson distribution
+	if (strcmp(argv[selector], "-poisson") == 0)  
+        	{
+	
+		for (i = 0; i < n; i++)
+			{
+			unsigned int k = gsl_ran_poisson(r,mu);
+			printf(" %u",k);
+			}	
+		}
+
+	if (strcmp(argv[selector], "-uniform") == 0) 
+        	{
+		//TODO: add the uniform distribution
+		}
+	
+	if (strcmp(argv[selector], "-other") == 0)  
+        	{
+		//TODO: add the third distribution
+		}
+	}	
 printf("\n");
 
 gsl_rng_free(r);
